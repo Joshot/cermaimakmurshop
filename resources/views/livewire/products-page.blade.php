f9b06b00ab2345e95e670e0c79977446333ac0fa
Joshua Hotama <joshuahh554@gmail.com>
2024-08-21 11:52:53 +0700

    up

--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -55,16 +55,28 @@
                         <h2 class="text-2xl font-bold dark:text-gray-400">Price</h2>
                         <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
                         <div>
-                            <div class="font-semibold">
-                                {{ Number::currency($price_range) }}
+                            <div class="flex items-center mb-2 space-x-2">
+                                <span class="font-semibold">
+                                    {{ Number::currency($price_range) }}
+                                </span>
                             </div>
-                            <input type="range" wire:model.live="price_range" class="w-full h-1 mb-4 bg-blue-100 rounded appearance-none cursor-pointer" max="500" value="250" step="0">
+                            <input type="number" wire:model.live="price_range"
+                                   class="w-20 p-1 text-center border border-gray-300 rounded"
+                                   min="0" max="500" step="1">
+                            <input type="range" wire:model.live="price_range"
+                                   class="w-full h-1 mb-4 bg-blue-100 rounded appearance-none cursor-pointer"
+                                   max="500" step="1">
                             <div class="flex justify-between ">
-                                <span class="inline-block text-lg font-bold text-blue-400 ">{{ Number::currency(0) }}</span>
-                                <span class="inline-block text-lg font-bold text-blue-400 ">{{ Number::currency(500) }}</span>
+                                <span class="inline-block text-lg font-bold text-blue-400 ">
+                                    {{ Number::currency(0) }}
+                                </span>
+                                <span class="inline-block text-lg font-bold text-blue-400 ">
+                                    {{ Number::currency(500) }}
+                                </span>
                             </div>
                         </div>
                     </div>
+
 
                 </div>
                 <div class="w-full px-3 lg:w-3/4">
