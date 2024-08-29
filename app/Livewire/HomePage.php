78b64e2efea3891cc692350a739ac49ad55a3452
Joshua Hotama <joshuahh554@gmail.com>
2024-08-29 09:28:34 +0700

    up

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -4,21 +4,22 @@ namespace App\Livewire;
 
 use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Product;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Title('Home Page - Cermaimakmur Shop')]
-
-
 class HomePage extends Component
 {
     public function render()
     {
         $brands = Brand::where('is_active', 1)->get();
         $categories = Category::where('is_active', 1)->get();
+        $products = Product::where('is_active', 1)->get();
         return view('livewire.home-page', [
             'brands' => $brands,
             'categories' => $categories,
+            'products' => $products,
         ]);
     }
 }
